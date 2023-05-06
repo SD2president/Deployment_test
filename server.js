746dff3d800757f4027e6396f698bec962223ca0
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2023-05-06 13:24:32 -0700

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+//Global
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(PORT, () => console.log('Server is awaked ${PORT}'))
+
+
+module.exports = app
